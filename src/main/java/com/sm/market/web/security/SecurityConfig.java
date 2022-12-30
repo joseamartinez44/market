3b1cacca1b588ab0150e8015997a08a5771126e0
joseamartinez44 <josep.castelan@gmail.com>
2022-12-29 19:26:32 -0600

    Configurar la seguridad de nuestra API con Spring Security

---
+++ src/main/java/com/sm/market/web/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.sm.market.web.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class SecurityConfig {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .and()
+                .build();
+    }
+}
